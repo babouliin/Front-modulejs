1dd9f19db9939b8ad6a6310249d936219cb5470b
François Debert <francois.debert@epitech.eu>
2022-01-02 18:23:39 +0100

    [FIX] comment socket.onAny

--- src/socket.js
+++ src/socket.js
@@ -3,9 +3,9 @@ import { io } from 'socket.io-client';
 const URL = process.env.REACT_APP_API_URL;
 const socket = io(URL, { autoConnect: false });
 
-socket.onAny((event, ...args) => {
-  // eslint-disable-next-line no-console
-  console.log(`Socket ${event}`, args);
-});
+// socket.onAny((event, ...args) => {
+//   // eslint-disable-next-line no-console
+//   console.log(`Socket ${event}`, args);
+// });
 
 export default socket;
